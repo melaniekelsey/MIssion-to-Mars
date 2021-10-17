c40f48fdce72e1f057e08aa52b1d5e9174c5d8c5
Jesse Kelsey <jessekelsey@Jesses-MacBook.local>
2021-10-17 12:44:11 -0700

    Adding py files

---
+++ app.py
@@ -0,0 +1,24 @@
+## Using flask to open a template and go to a website and create a website
+from flask import Flask, render_template, redirect, url_for
+## Using pymongo to work with Mongo DB
+from flask_pymongo import PyMongo
+## Scraping down we will convert from Jupyter notebook to Python
+import pandas as pd
+import scraping
+## Sets up flask
+app = Flask(__name__)
+# Use flask_pymongo to set up mongo connection, using a URI to connect
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return redirect('/', code=302)
+if __name__ == "__main__":
+   app.run()
